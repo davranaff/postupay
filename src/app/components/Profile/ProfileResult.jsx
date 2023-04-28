0d9947b31183ab805c92051c2b8f1c2de347c11b
Sukhrob <95549412+davranaff@users.noreply.github.com>
2023-04-28 21:29:06 +0000

    fix

--- src/app/components/Profile/ProfileResult.jsx
+++ src/app/components/Profile/ProfileResult.jsx
@@ -22,25 +22,25 @@ function ProfileResult() {
         <>
             {!loading ? data.length > 0 ? data.map((value, index) => {
                 const ball = Math.floor(100 * (value.score / (value.test_subject_histories.length / 4)))
-                return <div className={style.blockTest} key={index}>
-                    <div className={style.progress} key={value.id}>
-                        <div className={style.backGround} style={
-                            {
-                                width: `${ball}%`,
-                                backgroundColor: ball < 60 && '#F4DCD6'
-                            }
-                        }>
-                            <div className={style.wave1}></div>
-                            <div className={style.wave2}></div>
-                        </div>
-                        <Image src={'/icons/check.svg'} alt={'check'} width={0} height={0}/>
-                        <div className={style.progressContent}>
-                            <h2>Тест: {value.university.title[i18n.language] && value.university.title[i18n.language]}</h2>
-                            <p>{value.subject.title[i18n.language] && value.subject.title[i18n.language]}</p>
-                        </div>
-                        <h1>{ball}%</h1>
+                if (ball > 5) return <div className={style.blockTest} key={index}>
+                <div className={style.progress} key={value.id}>
+                    <div className={style.backGround} style={
+                        {
+                            width: `${ball}%`,
+                            backgroundColor: ball < 60 && '#F4DCD6'
+                        }
+                    }>
+                        <div className={style.wave1}></div>
+                        <div className={style.wave2}></div>
                     </div>
+                    <Image src={'/icons/check.svg'} alt={'check'} width={0} height={0}/>
+                    <div className={style.progressContent}>
+                        <h2>Тест: {value.university.title[i18n.language] && value.university.title[i18n.language]}</h2>
+                        <p>{value.subject.title[i18n.language] && value.subject.title[i18n.language]}</p>
+                    </div>
+                    <h1>{ball}%</h1>
                 </div>
+            </div> 
             }) : <ul style={{listStyleType: "none"}}>
                 <li>У Вас пока нет пройденных тестов :(</li>
             </ul> : <div className={style.loadingContainer}>
