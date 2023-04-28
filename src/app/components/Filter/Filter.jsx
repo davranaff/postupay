f80948edfa90f73362e42d41d00994bbf35d121e
shakhbozv <shakhbozv@gmail.com>
2023-04-29 03:02:48 +0500

    fixxed filter

--- src/app/components/Filter/Filter.jsx
+++ src/app/components/Filter/Filter.jsx
@@ -4,6 +4,9 @@ import FilterResult from "@/app/components/Filter/result/FilterResult";
 import FilterActions from "@/app/components/Filter/actions/FilterActions";
 import {filter} from "@/app/services/filter/filter";
 import {useRouter} from "next/router";
+import {element} from "prop-types";
+
+const PAGE_SIZE = 10;
 
 function Filter(props) {
     const [showSideBar, setShowSideBar] = useState(true)
@@ -12,21 +15,28 @@ function Filter(props) {
     const router = useRouter()
     const [loading, setLoading] = useState(true)
     const [miniLoading, setMiniLoading] = useState(false)
-    const [pagination, setPagination] = useState('&limit=10&offset=0')
+    const [pagination, setPagination] = useState({limit: PAGE_SIZE, offset: 0});
+    const [hasMore, setHasMore] = useState(true);
     const [allCount, setAllCount] = useState(null)
-
+    const [pArr, setPArr] = useState([])
     useEffect(_ => {
         if (Object.keys(router.query).length > 0) {
-            const arr = []
-            
+            let arr = []
+
             Object.keys(router.query).forEach((element) => {
                 if (typeof router.query[element] === "object") {
                     router.query[element].forEach(item => {
                         arr.push(`${element}=${item}`)
+                        setPArr(arr)
+                        setPagination({limit: PAGE_SIZE, offset: 0})
+                        // setArr([...arr,`${element}=${item}`])
                     })
                     return
                 }
                 arr.push(`${element}=${router.query[element]}`)
+                setPagination({limit: PAGE_SIZE, offset: 0})
+                setPArr(arr)
+                // setArr([...arr,`${element}=${router.query[element]}` ])
             })
             setMiniLoading(true)
             filter.getFilterResult(`${arr.join("&")}`, pagination).then(r => {
@@ -34,25 +44,65 @@ function Filter(props) {
                 setLoading(false)
                 setMiniLoading(false)
                 setAllCount(r.data.count)
+                setHasMore(r.data.next !== null);
             })
             return
         }
-        async function getData() {
-            setMiniLoading(true)
-            await filter.getFilterResult(pagination).then(res => {
-                setData(res.data.results)
-                setLoading(false)
-                setMiniLoading(false)
-                setAllCount(res.data.count)
-            })
+
+       else {
+            async function getData() {
+                setMiniLoading(true)
+                await filter.getFilterResult('', pagination).then(res => {
+                    setData(res.data.results)
+                    setLoading(false)
+                    setMiniLoading(false)
+                    setAllCount(res.data.count)
+                    setHasMore(res.data.next !== null);
+                })
+            }
+
+
+            getData()
+        }
+    }, [params, router.query])
+    const handleLoadMore = async () => {
+
+
+        if (!hasMore) {
+            return
+        } else {
+            setMiniLoading(true);
+            const newPagination = {...pagination};
+            console.log(newPagination)
+            newPagination.offset += PAGE_SIZE;
+            setPagination(newPagination);
+            await filter.getFilterResult( pArr.join('&'), newPagination).then(res => {
+                const newData = res.data.results;
+                setData([...data, ...newData]);
+                setMiniLoading(false);
+                setHasMore(res.data.next !== null);
+            });
         }
 
-        getData()
-    }, [params, pagination, router.query])
+    };
 
 
     return (
-        <FilterContext.Provider value={{allCount, pagination, setPagination,miniLoading, loading, setLoading, showSideBar, setShowSideBar, data, setData, setParams}}>
+        <FilterContext.Provider value={{
+            allCount,
+            pagination,
+            setPagination,
+            miniLoading,
+            loading,
+            setLoading,
+            showSideBar,
+            setShowSideBar,
+            data,
+            setData,
+            setParams,
+            handleLoadMore,
+            hasMore,
+        }}>
             <FilterResult/>
             <FilterActions {...props}/>
         </FilterContext.Provider>
